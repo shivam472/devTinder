8cb85028cd48bbe0f4c17579c57e4dcbbf2f185c
Shivam Pandey <shivam@trynarrative.com>
2026-04-30 00:20:20 +0530

    query params & dynamic routes

--- src/app.js
+++ src/app.js
@@ -1,30 +1,32 @@
-const express = require('express')
+const express = require("express");
 
 const app = express();
 
-app.get("/user", (req, res) => {
-    res.send("Hello Shivam!")
-})
+app.get("/user/:userId/:name", (req, res) => {
+  console.log(req.query); // query parameter: /user?id=123&name=shivam -> response { id: '123', name: 'shivam' }
+  console.log(req.params) // dynamic routes: /user/123/shivam -> response { userId: '123', name: 'shivam' }
+  res.send("Hello Shivam!");
+});
 
 app.post("/user", (req, res) => {
-    res.send("Successfully save user to the DB")
-})
+  res.send("Successfully save user to the DB");
+});
 
 app.patch("/user", (req, res) => {
-    res.send("Successfully updated the user in the DB")
-})
+  res.send("Successfully updated the user in the DB");
+});
 
 app.put("/user", (req, res) => {
-    res.send("Successfully overridden the user in the DB")
-})
+  res.send("Successfully overridden the user in the DB");
+});
 
 app.delete("/user", (req, res) => {
-    res.send("Successfully deleted the user from the DB")
-})
+  res.send("Successfully deleted the user from the DB");
+});
 app.get("/", (req, res) => {
-    res.send("Welcome Home!")
-})
+  res.send("Welcome Home!");
+});
 
 app.listen(7777, () => {
-    console.log("Server successfully listening on 7777...")
-})
+  console.log("Server successfully listening on 7777...");
+});
