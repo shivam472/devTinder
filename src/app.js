95aae710647409a6cac6964904d93c014e0c15e4
Shivam Pandey <shivam@trynarrative.com>
2026-04-30 17:37:25 +0530

    middleware

--- src/app.js
+++ src/app.js
@@ -2,9 +2,21 @@ const express = require("express");
 
 const app = express();
 
+// auth middleware
+app.use("/", (req, res, next) => {
+  const token = "xyz";
+  const isUserAuthenticated = token === "abc";
+  if (!isUserAuthenticated) {
+    res.status(401).send("Unauthorized request!");
+  } else {
+    next();
+  }
+});
+
+
 app.get("/user/:userId/:name", (req, res) => {
   console.log(req.query); // query parameter: /user?id=123&name=shivam -> response { id: '123', name: 'shivam' }
-  console.log(req.params) // dynamic routes: /user/123/shivam -> response { userId: '123', name: 'shivam' }
+  console.log(req.params); // dynamic routes: /user/123/shivam -> response { userId: '123', name: 'shivam' }
   res.send("Hello Shivam!");
 });
 
