0a5edc5c1d0e8cf6b4745fa16c94ad5c3be0f883
Shivam Pandey <shivam@trynarrative.com>
2026-05-02 13:06:58 +0530

    add user model and signup route for user registration

--- src/app.js
+++ src/app.js
@@ -2,8 +2,26 @@ const express = require("express");
 const { adminAuth, userAuth } = require("./middlewares/auth");
 const { errorMiddleware } = require("./middlewares/error");
 const { connectDB } = require("./database");
+const User = require("./models/user");
 
 const app = express();
+
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    firstName: "Shivam",
+    lastName: "Pandey",
+    email: "shivam@pandey.com",
+    password: "shivam@123",
+    gender: "male",
+  });
+
+  try {
+    await user.save();
+    res.send("Successfully added user to database");
+  } catch (err) {
+    res.status(400).send("Invalid request");
+  }
+});
 
 connectDB()
   .then(() => {
