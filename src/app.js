9a8e2cb81f562e7fe9161af1e1edd4c25a14387f
Shivam Pandey <shivam@trynarrative.com>
2026-05-04 09:41:27 +0530

    refactor user routes for improved error handling and response messages

--- src/app.js
+++ src/app.js
@@ -9,13 +9,68 @@ const app = express();
 app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-  const user = new User(req.body);
-
   try {
-    await user.save();
+    await User.create(req.body);
     res.send("Successfully added user to database");
   } catch (err) {
     res.status(400).send("Invalid request");
+  }
+});
+
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.email;
+  if (!userEmail) {
+    res.status(400).send("Invalid request!");
+  }
+
+  try {
+    const user = await User.findOne({ email: userEmail });
+    if (!user) {
+      res.status(404).send("User not found!");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong!");
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong!");
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  const userEmail = req.body.email;
+  try {
+    const result = await User.deleteOne({ email: userEmail });
+    if (result.deletedCount === 0) res.status(404).send("user not found!");
+    res.send("user deleted successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong!");
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  const userEmail = req.body.email;
+  const newEmail = req.body.newEmail;
+  try {
+    const result = await User.updateOne(
+      { email: userEmail },
+      {
+        email: newEmail,
+      },
+    );
+    if (result.matchedCount === 0)
+      return res.status(404).send("user not found!");
+
+    res.send("user updated successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong!");
   }
 });
 
